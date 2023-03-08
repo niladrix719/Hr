b199e246acc15b07ad75c141e2e390435ea8d9b8
Niladri <rickyrickado14@gmail.com>
2023-03-08 15:48:42 +0530

    added all files

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,36 @@
+class EmployeesController < ApplicationController
+	def index
+		@employees = Employee.all
+	end
+
+	def new
+		@employee = Employee.new
+	end
+
+	def create
+		@employee = Employee.new(employee_params)
+		if @employee.save
+			redirect_to employees_path , notice: 'Employee has been created Successfully!'
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@employee = Employee.find(params[:id])
+	end
+
+	def update
+		@employee = Employee.find(params[:id])
+		if @employee.update(employee_params)
+			redirect_to employees_path , notice: 'Employee has been updated Successfully!'
+		else
+			render :new
+		end
+	end
+
+	def employee_params
+		params.require(:employee).permit(:first_name , :middle_name , :last_name , :personal_email,
+			:city , :state , :country , :pincode , :address_line_1 , :address_line_2)
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root "home#index"
+
+  resources :employees
+
+  #public pages routes
+  get 'about' => 'pages#about_us'
+  get 'contact' => 'pages#contact_us'
+  get 'privacy-policy' => 'pages#privacy_policy'
+  get 'terms-and-conditions' => 'pages#terms_and_conditions'
+end
